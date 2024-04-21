f8c159bacd43df8a46bccc8b597b7bb228fcdcd2
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-04-21 17:36:40 +0900

    feature 모듈 의존성 추가
    data, domain의 mainPage와 의존성 추가했습니다.

--- feature/mainPage/build.gradle.kts
+++ feature/mainPage/build.gradle.kts
@@ -36,13 +36,15 @@ android {
 }
 
 dependencies {
+    implementation(project(":data:mainPage"))
+    implementation(project(":domain:mainPage"))
+    implementation(project(":core:ui"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(project(":core:ui"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
