50b6373f7226e392737e1c672dccaa3d7513eeab
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-04-21 23:03:49 +0900

    domain > data 의존성

--- domain/mainPage/build.gradle.kts
+++ domain/mainPage/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 }
 
 dependencies {
-
+    implementation(project(":data:mainPage"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
