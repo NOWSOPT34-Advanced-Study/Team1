e95de77e6fc30a771f8c7d360341a385095fc7d3
Dongmin <pdm001125@naver.com>
2024-04-25 02:40:44 +0900

    #7/feat: Preferences DataStore 필수 값 get/set 설정

--- data/local/src/main/java/com/simhwa/data/local/LocalDataStore.kt
+++ data/local/src/main/java/com/simhwa/data/local/LocalDataStore.kt
@@ -3,10 +3,18 @@ package com.simhwa.data.local
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import com.simhwa.data.local.model.UserInfo
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ActivityRetainedScoped
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+import java.io.IOException
 import javax.inject.Inject
 
 private const val DATASTORE_NAME = "local_datastore"
@@ -19,13 +27,38 @@ class LocalDataStore @Inject constructor(
 ) {
     private object Keys {
         val id = stringPreferencesKey(ID_KEY)
-        val password = stringPreferencesKey(PW_KEY)
+        val password = stringPreferencesKey(PASSWORD_KEY)
         val nickname = stringPreferencesKey(NICKNAME_KEY)
+    }
+
+    val userInfo: Flow<UserInfo> = context.localDataStore.data
+        .catch {
+            if (it is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw it
+            }
+        }
+        .map { preferences ->
+            UserInfo(
+                id = preferences[Keys.id].orEmpty(),
+                password = preferences[Keys.password].orEmpty(),
+                nickname = preferences[Keys.nickname].orEmpty(),
+            )
+        }
+        .distinctUntilChanged()
+
+    suspend fun setUserInfo(userInfo: UserInfo) {
+        context.localDataStore.edit { preferences ->
+            preferences[Keys.id] = userInfo.id
+            preferences[Keys.password] = userInfo.password
+            preferences[Keys.nickname] = userInfo.nickname
+        }
     }
 
     companion object {
         const val ID_KEY = "id_key"
-        const val PW_KEY = "pw_key"
+        const val PASSWORD_KEY = "pw_key"
         const val NICKNAME_KEY = "nickname_key"
     }
 }
---
+++ data/local/src/main/java/com/simhwa/data/local/model/UserInfo.kt
@@ -0,0 +1,7 @@
+package com.simhwa.data.local.model
+
+data class UserInfo(
+    val id: String,
+    val password: String,
+    val nickname: String,
+)
