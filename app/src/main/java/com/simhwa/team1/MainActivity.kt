6290c0de782b81171bbfc43cb68823e438a034d9
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-04-29 20:51:07 +0900

    mainActivity에 fragment 초기화
    mainActivity 실행하면 signin 모듈의 프래그먼트가 실행됩니다.

--- app/src/main/java/com/simhwa/team1/MainActivity.kt
+++ app/src/main/java/com/simhwa/team1/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.simhwa.team1
 
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.simhwa.team1.databinding.ActivityMainBinding
 import com.simhwa.ui.base.BaseActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>({ ActivityMainBinding.inflate(it) }) {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initFragmentContainerView()
+    }
 
-        setContentView(binding.root)
+    private fun initFragmentContainerView() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
+        navController = navHostFragment.navController
+        navController.graph.setStartDestination(com.simhwa.signin.R.id.navigation_graph_sign_in)
     }
 }
