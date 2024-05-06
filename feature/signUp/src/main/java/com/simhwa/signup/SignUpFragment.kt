99813e86e688b4d0acfb323b299017f1d1fd49f5
LEE YOU BIN <youbeene3409@naver.com>
2024-05-06 23:25:17 +0900

    [FEAT/#11] 스택 제거

--- feature/signUp/src/main/java/com/simhwa/signup/SignUpFragment.kt
+++ feature/signUp/src/main/java/com/simhwa/signup/SignUpFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.net.toUri
 import androidx.navigation.NavDeepLinkRequest
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import com.simhwa.signup.databinding.FragmentSignUpBinding
 import com.simhwa.ui.base.BaseFragment
@@ -18,6 +19,7 @@ class SignUpFragment : BaseFragment<FragmentSignUpBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         initSignUpBtnClickListener()
     }
 
@@ -26,12 +28,12 @@ class SignUpFragment : BaseFragment<FragmentSignUpBinding>() {
             val request = NavDeepLinkRequest.Builder
                 .fromUri("featureSignIn://SignInFragment".toUri())
                 .build()
-            findNavController().navigate(request)
+
+            val navOptions = NavOptions.Builder()
+                .setPopUpTo(findNavController().graph.startDestinationId, true)
+                .build()
+
+            findNavController().navigate(request, navOptions)
         }
     }
 }
-
-
-
-
-
