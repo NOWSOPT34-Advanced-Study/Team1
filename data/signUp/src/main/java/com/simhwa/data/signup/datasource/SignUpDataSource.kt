d3f9a54c0404ae41214ed3e5d32faf371aaf6aa3
LEE YOU BIN <youbeene3409@naver.com>
2024-05-07 17:23:11 +0900

    [FEAT/#11] 데이터소스 구현

--- data/signUp/src/main/java/com/simhwa/data/signup/datasource/SignUpDataSource.kt
+++ data/signUp/src/main/java/com/simhwa/data/signup/datasource/SignUpDataSource.kt
@@ -4,9 +4,6 @@ import com.simhwa.data.local.datastore.LocalUserDataStore
 import com.simhwa.data.local.model.UserInfo
 import javax.inject.Inject
 
-class SignUpDataSource @Inject constructor(private val localUserDataStore: LocalUserDataStore) {
-
-    suspend fun signUpUser(user: UserInfo) {
-        localUserDataStore.setUserInfo(user)
-    }
+interface SignUpDataSource{
+    suspend fun signUpUser(user: UserInfo)
 }
