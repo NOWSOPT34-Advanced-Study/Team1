0dbdee856d3e9274a63cca149c248a8a386c93a3
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-05-13 14:45:44 +0900

    memopage 정보받아오기

---
+++ feature/memoPage/src/main/java/com/simhwa/mainPage/MemoPageViewModel.kt
@@ -0,0 +1,24 @@
+package com.simhwa.mainPage
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.simhwa.data.mainpage.model.User
+import com.simhwa.data.mainpage.repository.MainPageRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MemoPageViewModel @Inject constructor(private val repository: MainPageRepository) :
+    ViewModel() {
+    private var _userInfo: MutableLiveData<User> = MutableLiveData(User("", "", ""))
+    val userInfo: LiveData<User> get() = _userInfo
+
+    fun getUserInfo() {
+        viewModelScope.launch {
+            _userInfo.value = repository.mainPageUSer()
+        }
+    }
+}
